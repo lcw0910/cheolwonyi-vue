78adce1f30734480f00a6aff79fa16cd3c3b7119
Cheolwon Lee <chlwn.lee@gmail.com>
2019-04-10 11:20:35 +0900

    Layout Default

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,82 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <!--<div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <router-view/>-->
+
+    <!-- {navigation} -->
+    <nav class="scrollbar">
+      <h1>Nav</h1>
+      <ul class="list-group">
+        <li class="list-group-item">nav1</li>
+        <li class="list-group-item">nav2</li>
+        <li class="list-group-item">nav3</li>
+        <li class="list-group-item">nav3</li>
+        <li class="list-group-item">nav3</li>
+        <li class="list-group-item">nav3</li>
+        <li class="list-group-item">nav3</li>
+        <li class="list-group-item">nav3</li>
+        <li class="list-group-item">nav3</li>
+        <li class="list-group-item">nav3</li>
+        <li class="list-group-item">nav3</li>
+        <li class="list-group-item">nav3</li>
+        <li class="list-group-item">nav3</li>
+        <li class="list-group-item">nav3</li>
+        <li class="list-group-item">nav3</li>
+        <li class="list-group-item">nav3</li>
+        <li class="list-group-item">nav3</li>
+        <li class="list-group-item">nav3</li>
+        <li class="list-group-item">nav3</li>
+        <li class="list-group-item">nav3</li>
+        <li class="list-group-item">nav3</li>
+        <li class="list-group-item">nav3</li>
+        <li class="list-group-item">nav3</li>
+        <li class="list-group-item">nav3</li>
+        <li class="list-group-item">nav3</li>
+        <li class="list-group-item">nav3</li>
+        <li class="list-group-item">nav3</li>
+        <li class="list-group-item">nav3</li>
+        <li class="list-group-item">nav3</li>
+        <li class="list-group-item">nav3</li>
+        <li class="list-group-item">nav3</li>
+        <li class="list-group-item">nav3</li>
+      </ul>
+    </nav>
+    <!--//{navigation}-->
+
+    <!--{container}-->
+    <div id="container">
+
+      <header> </header>
+
+      <div id="contents">
+        <section></section>
+        <aside class="scrollbar">
+          <div>
+            <h1>Aside</h1>
+            <ul>
+              <li>aside1</li>
+              <li>aside2</li>
+              <li>aside3</li>
+              <li>aside3</li>
+              <li>aside3</li>
+              <li>aside3</li>
+              <li>aside3</li>
+            </ul>
+          </div>
+        </aside>
+      </div>
+
+      <footer></footer>
+
+    </div>
+    <!--//{container}-->
   </div>
 </template>
+
+
 
 <style lang="scss">
 @import "styles/global";
---
+++ src/template/test1/App.vue
@@ -0,0 +1,119 @@
+<template>
+    <div id="app">
+        <!--<div id="nav">
+          <router-link to="/">Home</router-link> |
+          <router-link to="/about">About</router-link>
+        </div>
+        <router-view/>-->
+
+        <!-- {wrapper} -->
+        <div id="wrapper">
+
+            <!--{header}-->
+            <header> </header>
+            <!--//{header}-->
+
+            <!--{container}-->
+            <div id="container">
+
+                <!--{container}-->
+                <nav class="scrollbar">
+                    <h1>Nav</h1>
+                    <ul>
+                        <li>nav1</li>
+                        <li>nav2</li>
+                        <li>nav3</li>
+                        <li>nav3</li>
+                        <li>nav3</li>
+                        <li>nav3</li>
+                        <li>nav3</li>
+                        <li>nav3</li>
+                        <li>nav3</li>
+                        <li>nav3</li>
+                        <li>nav3</li>
+                        <li>nav3</li>
+                        <li>nav3</li>
+                        <li>nav3</li>
+                        <li>nav3</li>
+                        <li>nav3</li>
+                        <li>nav3</li>
+                        <li>nav3</li>
+                        <li>nav3</li>
+                        <li>nav3</li>
+                        <li>nav3</li>
+                        <li>nav3</li>
+                        <li>nav3</li>
+                        <li>nav3</li>
+                        <li>nav3</li>
+                        <li>nav3</li>
+                        <li>nav3</li>
+                        <li>nav3</li>
+                        <li>nav3</li>
+                        <li>nav3</li>
+                        <li>nav3</li>
+                        <li>nav3</li>
+                    </ul>
+                </nav>
+                <!--//{container}-->
+
+                <!--{section}-->
+                <section>
+                </section>
+                <!--//{section}-->
+
+                <!--{aside}-->
+                <aside class="scrollbar">
+                    <div>
+                        <h1>Nav</h1>
+                        <ul>
+                            <li>nav1</li>
+                            <li>nav2</li>
+                            <li>nav3</li>
+                            <li>nav3</li>
+                            <li>nav3</li>
+                            <li>nav3</li>
+                            <li>nav3</li>
+                            <li>nav3</li>
+                            <li>nav3</li>
+                            <li>nav3</li>
+                            <li>nav3</li>
+                            <li>nav3</li>
+                            <li>nav3</li>
+                            <li>nav3</li>
+                            <li>nav3</li>
+                            <li>nav3</li>
+                            <li>nav3</li>
+                            <li>nav3</li>
+                            <li>nav3</li>
+                            <li>nav3</li>
+                            <li>nav3</li>
+                            <li>nav3</li>
+                            <li>nav3</li>
+                            <li>nav3</li>
+                            <li>nav3</li>
+                            <li>nav3</li>
+                            <li>nav3</li>
+                            <li>nav3</li>
+                            <li>nav3</li>
+                            <li>nav3</li>
+                            <li>nav3</li>
+                            <li>nav3</li>
+                        </ul>
+                    </div>
+                </aside>
+                <!--//{section}-->
+
+            </div>
+            <!--//{container}-->
+
+            <!--{footer}-->
+            <footer></footer>
+            <!--//{footer}-->
+        </div>
+        <!-- //{wrapper} -->
+    </div>
+</template>
+
+<style lang="scss">
+    @import "styles/global";
+</style>
